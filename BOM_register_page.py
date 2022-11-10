df8a3300187ca030bc5d56993e5be6624a74a20c
taewonyang <59527808+taewonyang@users.noreply.github.com>
2022-11-11 00:17:32 +0900

    Add files via upload

--- BOM_register_page.py
+++ BOM_register_page.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter.ttk as ttk
 import tkinter.messagebox as msgbox
 import sqlite3
+from tkcalendar import Calendar
 
 class Bom_register():
     def __init__(self, window):
@@ -11,40 +12,58 @@ class Bom_register():
         self.layout()
 
     def layout(self):
-        topFrame = Frame(self.window, width=1500, height=70, bg='#FDFFFF')
+        topFrame = Frame(self.window, width=1500, height=70, bg='#FDFFFF', bd=0.5)
         topFrame.place(x=0, y=0)
         leftFrame = Frame(self.window, width=600, height=630, relief='solid', bd=0.5)
-        leftFrame.place(x=0, y=70)
-        rightFrame = Frame(self.window, width=900, height=630)
-        rightFrame.place(x=600, y=70)
+        leftFrame.place(x=5, y=70)
+        rightFrame = Frame(self.window, width=885, height=630, bd=0.5)
+        rightFrame.place(x=610, y=70)
 
         # topFrame내용
         title = Label(topFrame, text='BOM 등록', font=("Georgia", 15), bg='#FDFFFF')
         title.place(x=30, y=20)
 
         # leftFrame내용
-        bomInfo_frame = LabelFrame(leftFrame, text='기본 정보')
-        bomInfo_frame.place(x=20, y=20)
-        bomDate_lb = Label(bomInfo_frame, text='입력 날짜          \n(ex:2022-11-10)')
+        # -기본 정보(공통)-
+        bomInfo_frame = LabelFrame(leftFrame, text='기본 정보(공통)', labelanchor=N)
+        bomInfo_frame.pack(fill='both', expand=True, padx=10, pady=10)
+        innerLeft_frame = Frame(bomInfo_frame)
+        innerLeft_frame.grid(row=0, column=0, padx=10)
+        innerRight_frame = Frame(bomInfo_frame)
+        innerRight_frame.grid(row=0, column=1, padx=10)
+
+        bomDate_lb = Label(innerLeft_frame, text='날짜 입력       \n(ex:2022-11-10)')
         bomDate_lb.grid(row=0, column=0, padx=5, pady=3, sticky=W)
-        bomDate_lbframe = Label(bomInfo_frame)
+        bomDate_lbframe = Label(innerLeft_frame)
         bomDate_lbframe.grid(row=0, column=1, padx=5, pady=3, sticky=W)
         bomDate_e = Entry(bomDate_lbframe, width=10)
         bomDate_e.grid(row=0, column=0,padx=1)
         bomToday_btn = Button(bomDate_lbframe, text=' T ')
         bomToday_btn.grid(row=0, column=1, padx=1)
-        bomCurrent_lb = Label(bomInfo_frame, text='통화')
+        bomCurrent_lb = Label(innerLeft_frame, text='통화')
         bomCurrent_lb.grid(row=1, column=0, padx=5, pady=3, sticky=W)
-        bomCurrent_cmb = ttk.Combobox(bomInfo_frame, height=5, width=8, state='readonly')
+        bomCurrent_cmb = ttk.Combobox(innerLeft_frame, height=5, width=8, state='readonly')
         bomCurrent_cmb.grid(row=1, column=1, padx=5, pady=3, sticky=W)
-        bomExRate_lb = Label(bomInfo_frame, text='환율')
-        bomExRate_lb.grid(row=1, column=2, padx=5, pady=3, sticky=W)
-        bomExRate_e = Entry(bomInfo_frame, width=10)
-        bomExRate_e.grid(row=1, column=3, padx=5, pady=3, sticky=W)
+        bomExRate_lb = Label(innerLeft_frame, text='환율')
+        bomExRate_lb.grid(row=2, column=0, padx=5, pady=3, sticky=W)
+        bomExRate_e = Entry(innerLeft_frame, width=10)
+        bomExRate_e.grid(row=2, column=1, padx=5, pady=3, sticky=W)
+        CheckVar1 = IntVar()
+        keep_chkbtn = Checkbutton(innerLeft_frame, text="입력내용 유지", variable=CheckVar1)
+        keep_chkbtn.grid(row=3, column=0, padx=7, pady=7)
+        clear_btn = Button(innerLeft_frame, text=' 기본정보 \n Clear ')
+        clear_btn.grid(row=3, column=1, padx=7, pady=7)
 
+        cal1 = Calendar(innerRight_frame, selectmode='day')
+        cal1.grid(row=0, column=0, padx=3, pady=5)
+        # cal1.pack(side='left', padx=10, pady=(30, 10))
+
+        # -세부정보-
+        detailInfo_frame = LabelFrame(leftFrame, text='세부 내용', labelanchor=N)
+        detailInfo_frame.pack(fill='both', expand=True, padx=10, pady=15)
         #제품정보
-        producInfo_frame = LabelFrame(leftFrame, text='제품 정보')
-        producInfo_frame.place(x=20, y=130)
+        producInfo_frame = LabelFrame(detailInfo_frame, text='제품 정보')
+        producInfo_frame.grid(row=0, column=0, padx=5, pady=10)
         productSerial_lb = Label(producInfo_frame, text='완제품 품번')
         productSerial_lb.grid(row=0, column=0, padx=5, pady=3)
         productSerial_txt = Label(producInfo_frame, width=8)
@@ -79,8 +98,8 @@ class Bom_register():
         productPrice_e.grid(row=3, column=1, padx=5, pady=3, sticky=W)
 
         #소재정보
-        materialInfo_frame = LabelFrame(leftFrame, text='소재 정보')
-        materialInfo_frame.place(x=20, y=280)
+        materialInfo_frame = LabelFrame(detailInfo_frame, text='소재 정보')
+        materialInfo_frame.grid(row=1, column=0, padx=5, pady=10, sticky=W)
         material_lb = Label(materialInfo_frame, text='원석')
         material_lb.grid(row=0, column=0, padx=5, pady=3)
         material_cmb = ttk.Combobox(materialInfo_frame, height=5, width=15, state='readonly')
@@ -108,5 +127,39 @@ class Bom_register():
         etcBuyDate_cmb.grid(row=2, column=3, padx=5, pady=3)
 
         # 버튼frmae
-        dataBtn_frame = LabelFrame(leftFrame, text='BOM 데이터')
-        dataBtn_frame.place(x=20, y=450)
+        dataBtn_frame = Frame(leftFrame)
+        dataBtn_frame.pack( fill='both', expand=True, padx=10, pady=10)
+
+        empty_lb1 = Label(dataBtn_frame, text='     ')
+        empty_lb1.grid(row=0,column=0, padx=10, pady=3)
+        empty_lb2 = Label(dataBtn_frame, text='     ')
+        empty_lb2.grid(row=0, column=1, padx=10, pady=3)
+        empty_lb3 = Label(dataBtn_frame, text='     ')
+        empty_lb3.grid(row=0, column=2, padx=10, pady=3)
+        new_frame = LabelFrame(dataBtn_frame, text='신규등록')
+        new_frame.grid(row=0, column=3, padx=10, pady=3)
+        db_insert_btn = Button(new_frame, text='  Save  ')
+        db_insert_btn.grid(row=0, column=0, padx=1, pady=3)
+
+        existing_Frame = LabelFrame(dataBtn_frame, text='기존데이터')
+        existing_Frame.grid(row=0, column=4, padx=10, pady=3)
+        btn_dataload_btn = Button(existing_Frame, text=' Load ')
+        btn_dataload_btn.grid(row=0, column=0, padx=1, pady=3)
+        cancel_btn = Button(existing_Frame, text='Cancel')
+        cancel_btn.grid(row=0, column=1, padx=1, pady=3)
+        db_delete_btn = Button(existing_Frame, text='  Del  ', fg='#FF0000')
+        db_delete_btn.grid(row=0, column=2, padx=9, pady=3)
+        db_edit_btn = Button(existing_Frame, text=' Update ')
+        db_edit_btn.grid(row=0, column=3, padx=1, pady=3)
+
+        global tree_frame
+        # rightFrame 내용
+        tree_frame = Frame(rightFrame, bg='blue')
+        tree_frame.place(x=10, y=10, width=870, height=610)
+
+    def create_tree_widget(self):
+        columns = ('sn_col', 'name_col', 'namecode_eng_col', 'namecode_kor_col', 'material_kind_col', 'hscode_col',
+                   'amount_col', 'ekw_col', 'manufacturer_col', 'country_origin_col', 'vendor_name_col', 'buydate_col',
+                   'exchange_col', 'price_col', 'current_col', 'total_price_col', 'document_col',
+                   'purchase_doc_valid_col',
+                   'origin_doc_valid_col', 'btn_col')
